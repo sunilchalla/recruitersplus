abe27d1131fbb9c24b29e306bf408c6d7bb70e82
chand494 <shaikchandbasha@scriptbees.com>
2023-09-07 16:22:13 +0530

    adding skeleton for Meeting Preview

--- src/pages/previewMeeting/PreviewMeeting.jsx
+++ src/pages/previewMeeting/PreviewMeeting.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 const PreviewMeeting = () => {
   return (
-    <div>PreviewMeeting</div>
-  )
+    <div>
+      <div className="row">
+        <div className="col-9">
+          <div className="row">
+            <h3> Meeting Id</h3>
+          </div>
+          <div className="row">
+            Job Details
+          </div>
+          <div className="row">
+            Hiring Progress
+          </div>
+          <div className="row">
+            Interview Details
+          </div>
+          <div className="row">
+            Job Description
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="col">Issue Details</div>
+          <div className="col">Attachments</div>
+          <div className="col">Interview Notes</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PreviewMeeting
